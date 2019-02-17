905439097e82208fe005824e946834c472779e55
Tal Yaron <tal.yaron@gmail.com>
2019-02-17 14:04:02 +0200

    fixed conection

--- server.js
+++ server.js
@@ -23,10 +23,54 @@ app.use(bodyParser.urlencoded({
 
 
 
+app.get('/set1', (req, res) => {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("nodechef-mongo");
+        dbo.createCollection("customers", function (err, res) {
+            if (err) throw err;
+            console.log("Collection created!");
+            db.close();
+        });
+    });
+})
+
+
+
+app.get('/set2', (req, res) => {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("nodechef-mongo");
+        var myobj = [
+            { name: 'John', address: 'Highway 71' },
+            { name: 'Peter', address: 'Lowstreet 4' },
+            { name: 'Amy', address: 'Apple st 652' },
+            { name: 'Hannah', address: 'Mountain 21' },
+            { name: 'Michael', address: 'Valley 345' },
+            { name: 'Sandy', address: 'Ocean blvd 2' },
+            { name: 'Betty', address: 'Green Grass 1' },
+            { name: 'Richard', address: 'Sky st 331' },
+            { name: 'Susan', address: 'One way 98' },
+            { name: 'Vicky', address: 'Yellow Garden 2' },
+            { name: 'Ben', address: 'Park Lane 38' },
+            { name: 'William', address: 'Central st 954' },
+            { name: 'Chuck', address: 'Main Road 989' },
+            { name: 'Viola', address: 'Sideway 1633' }
+        ];
+        dbo.collection("customers").insertMany(myobj, function (err, res) {
+            if (err) throw err;
+            console.log("Number of documents inserted: " + res.insertedCount);
+            db.close();
+        });
+    });
+})
+
+
+
 app.get('/api', (req, res) => {
     MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
-        var dbo = db.db("mydb");
+        var dbo = db.db("nodechef-mongo");
 
         var mysort = { name: -1 };
         dbo.collection("customers").find().sort(mysort).toArray(function (err, result) {
